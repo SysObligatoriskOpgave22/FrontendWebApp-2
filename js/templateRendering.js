239758b8e7af0d286ca9c0507e742740beb66a75
Malthe <malthegram22@gmail.com>
2022-03-31 09:40:36 +0200

    first commit

---
+++ js/templateRendering.js
@@ -0,0 +1,19 @@
+import { renderTemplate, setActive, showPage } from "./Utility.js"
+
+function renderMenuItems(evt) {
+    const element = evt.target
+    setActive(element)
+    const id = element.id;
+    renderTemplate(id)  //This setups the HTML for the page
+    switch (id) {
+        //Here you can execute JavaScript for the selected page
+        case "about" : {
+            console.log("home")
+            break
+        }
+    }
+}
+
+document.getElementById("menu").onclick = renderMenuItems;
+showPage("about") //Set the default page to render
+
