14bca769835fc5ff801114718e23e6e8fd1bd28a
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2022-04-05 13:12:53 +0200

    Added reset btn

--- js/templateRendering.js
+++ js/templateRendering.js
@@ -1,6 +1,6 @@
 import { renderTemplate, setActive, showPage } from "./Utility.js"
 import { category, drawMatrix, numbers } from "./matrix.js";
-import { cardsHandler } from "./fetchACard.js"
+import { cardsHandler, fetchRandomDeck} from "./fetchACard.js"
 
 function renderMenuItems(evt) {
     const element = evt.target
@@ -23,6 +23,8 @@ function renderMenuItems(evt) {
             console.log("quiz")
             cardsHandler()
             document.getElementById("next").onclick = test
+            document.getElementById("reset").onclick = fetchRandomDeck()
+
             break
         }
     }
